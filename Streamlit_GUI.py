6148db2f1865b224993607610ea0b979b46d5e2e
Aidan Murphy <Aidan.Murphy14@gmail.com>
2023-05-30 09:59:17 -0400

    still making changes to tweets

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -99,7 +99,7 @@ def main():
             col1.write(info['summary'])
 
             # get wiki info
-            wiki_url = get_wiki_info(info['longName'])
+            wiki_url = get_wiki_info(info['longname'])
             col1.write("Wikipedia URL:")
             col1.write(wiki_url)
 
