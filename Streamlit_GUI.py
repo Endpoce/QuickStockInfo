91143620a3481af76a4ed67084b689f93e35047d
Aidan Murphy <Aidan.Murphy14@gmail.com>
2023-06-20 08:07:58 -0400

    fixing business info to handle ETFs and indexes

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -81,9 +81,9 @@ def main():
         with col1.container():
             col1.write("Company Info:")
             col1.write(info['longName'])
-            if info['sector'] != None:
+            if info['sector']:
                 col1.write("Sector: "+info['sector'])
-            if info['industry'] != None:
+            if info['industry']:
                 col1.write("Industry: " + info['industry'])
             col1.write(info['longBusinessSummary'])
 
