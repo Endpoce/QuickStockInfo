cb1cddc4e1dd7e201376cdbb6e5af73cab70e16a
Aidan Murphy <Aidan.Murphy14@gmail.com>
2023-10-03 14:04:06 -0400

    fixing get_stock_data error

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -98,7 +98,7 @@ def main():
         col1, col2 = st.columns((1, 2))
 
         # Main Page
-        if fetch_button.clicked:
+        if fetch_button:
             try:
                 # download and save stock data
                 ticker, info, hist, file = get_stock_data(
