02d8d90b1bb0ea3f5a43021757fdeabd711c47b1
Aidan Murphy <AIdan.Murphy14@gmail.com>
2023-11-20 12:18:40 -0500

    Reorder imports and fix formatting in Streamlit_GUI.py

--- GUIs/Streamlit_GUI.py
+++ GUIs/Streamlit_GUI.py
@@ -1,17 +1,18 @@
 # Authors: Aidan Murphy
 # Date Created: 5/15/23
 
-import os
-from Gen_Files import *
-from dotenv import load_dotenv
-import openai
-import pandas as pd
-import yfinance as yf
-import streamlit as st
-from datetime import timedelta, datetime
 import sys
 
 sys.path.append('Gen_Files')
+
+from datetime import timedelta, datetime
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import openai
+from dotenv import load_dotenv
+from Gen_Files import *
+import os
 
 
 load_dotenv()
