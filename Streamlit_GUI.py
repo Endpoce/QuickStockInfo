253571d0767671d8715833a3eb2b4d4254a9c9d1
Aidan Murphy <AIdan.Murphy14@gmail.com>
2024-03-26 10:21:49 -0400

    Setting up tab 4 with google gemini

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -56,6 +56,7 @@ with st.sidebar:
     if fetch_button or primary_ticker != "":
         try:
             # display company info
+            global ticker, info, hist, symbol
             ticker, info, hist, symbol = get_stock_data(primary_ticker, start_date, end_date)
 
             # get ytd data
