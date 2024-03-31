8f161eb49c1642637041cc17dc3a2428dc3d7d0a
Aidan Murphy <AIdan.Murphy14@gmail.com>
2024-03-31 19:52:23 -0400

    fixing efficient frontier

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -165,7 +165,7 @@ with tab3:
     
     try:
         # display the efficient frontier
-        st.plotly_chart(get_efficient_frontier(1000, stock_data))
+        st.plotly_chart(get_efficient_frontier(10000, stock_data))
     
     except Exception as e:
         error_message(e)
