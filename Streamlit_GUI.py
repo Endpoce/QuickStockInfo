eabcad6441abd544cfd3e233e09c9be407528a46
Aidan Murphy <AIdan.Murphy14@gmail.com>
2024-04-22 19:35:45 -0400

    fixing max sharpe port

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -182,7 +182,8 @@ with tab3:
             # display the weights from the max sharpe portfolio
             st.write("Weights:")
             for key, value in max_sharpe_portfolio['Weights']:
-                st.write(key + ": " + str(value))
+                if key in tickers:
+                    st.write(key + ": " + str(value))
             
 
         with col2:
@@ -201,7 +202,8 @@ with tab3:
             # display the weights of each stock from the min volatility portfolio
             st.write("Weights:")
             for key, value in min_volatility_portfolio['Weights']:
-                st.write(key + ": " + str(value))
+                if key in tickers:
+                    st.write(key + ": " + str(value))
 
     
     except Exception as e:
