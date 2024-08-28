f52d2bae4e9f6958caaa33a4492c88db6abff995
Aidan <aidan.murphy14@gmail.com>
2024-08-28 16:47:15 -0400

    Fixing EF

--- Streamlit_GUI.py
+++ Streamlit_GUI.py
@@ -12,6 +12,9 @@ from Gen_Files.Stock_Analyzer import *
 import streamlit as st
 from Gen_Files.Gen_Funcs import *
 
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE, SIG_DFL)
+
 
 # set env vars
 load_dotenv()
